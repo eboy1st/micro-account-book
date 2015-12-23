05908701aa5b1932ef4ac9a275e442f3f74d53a9
XiaoHongwei <wangyixhw@163.com>
2015-12-23 23:00:36 +0800

    edit db.py

---
+++ www/db.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Xiao Hongwei'
+
+'''
+
+'''
+
+import 
+
+class agfga(object):
+    '''
+    ag
+    '''
+    def __init__(self,name):
+        pass
+        class (object):
+            '''
+            
+            '''
+            def __init__(self):
+                pass
+
+if __name__=='__main__':
+    pass	
